a98d500dd14967fb05af585ce9cd18ecba83846f
vakulenkoalex <vakulenko_alex@mail.ru>
2026-04-28 09:45:22 +0400

    убрал deprication

--- app/src/main/java/com/mobilemoney/ui/theme/Theme.kt
+++ app/src/main/java/com/mobilemoney/ui/theme/Theme.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -75,7 +74,7 @@ fun MobileMoneyTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.setDecorFitsSystemWindows(window, false)
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
